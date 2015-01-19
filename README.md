4757eb627f06a84a1e644503558007d21542b70a
Julius van der Vaart <studio@juliusvaart.com>
2015-01-19 18:48:55 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # mobile-menu
+A simple mobile menu based on jQuery and optional GSAP JS.
---
+++ js/script.js
@@ -0,0 +1,9 @@
+(function ($) {
+  
+  $('body').jvmobilemenu({
+    notMenuClick: $('.page'),
+    slideSpeed: 0.3,
+    menuWidth: 240
+  });
+  
+})(jQuery);
